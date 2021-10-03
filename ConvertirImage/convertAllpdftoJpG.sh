11a34bc390dbf19346589235e8cfa1175ffa54de
SLamasse <74814681+SLamasse@users.noreply.github.com>
2021-10-03 18:46:22 +0200

    Add files via upload

---
+++ ConvertirImage/convertAllpdftoJpG.sh
@@ -0,0 +1,6 @@
+for inputfile in ./*.pdf ; do
+    outputfile="${inputfile%.*}.jpg"
+    convert  -sharpen 0x4 -verbose -density 150 -trim "$inputfile" -quality 100  -resize 900x900 "$outputfile" &&
+    [[ -e "$outputfile" && "$inputfile" != "$outputfile" ]] && rm "$inputfile"
+done
+
