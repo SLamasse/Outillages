55680201132001ecd955bd7ad49fc0b6544ce7ef
SLamasse <74814681+SLamasse@users.noreply.github.com>
2021-10-03 19:00:35 +0200

    Update convertAllpdftoJpG.sh

--- ConvertirImage/convertAllpdftoJpG.sh
+++ ConvertirImage/convertAllpdftoJpG.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for inputfile in ./*.pdf ; do
     outputfile="${inputfile%.*}.jpg"
     convert  -sharpen 0x4 -verbose -density 150 -trim "$inputfile" -quality 100  -resize 900x900 "$outputfile" &&
